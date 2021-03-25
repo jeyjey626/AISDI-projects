88ce680e4a48dd4768cb30584c44a332730f2359
akudzia <01167587@pw.edu.pl>
2021-03-25 16:58:29 +0000

    Replace test_sorting_algorithms.py

--- sorting-algorithms/test_sorting_algorithms.py
+++ sorting-algorithms/test_sorting_algorithms.py
@@ -1,8 +1,5 @@
 from unittest import TestCase
-from src import bubble_sort
-from src import quick_sort
-from src import merge_sort
-from src import counting_sort
+from src import quick_sort, bubble_sort, merge_sort, counting_sort
 
 
 class Test(TestCase):
@@ -25,24 +22,24 @@ class Test(TestCase):
 
     def test_quick_sort(self):
         data = [29, 3, 92, 2, 4]
-        self.assertListEqual(sorted(data), quick_sort.quick_sort(data, 0, len(data) - 1))
+        self.assertListEqual(sorted(data), quick_sort.sort(data))
 
     def test_quick_sort_chars(self):
         data = ['z', '2', ',', ' ', 'o']
-        self.assertListEqual(sorted(data), quick_sort.quick_sort(data, 0, len(data) - 1))
+        self.assertListEqual(sorted(data), quick_sort.sort(data))
 
     # Merge sorting tests
 
     def test_merge_sort(self):
         data = [29, 3, 92, 2, 4]
-        self.assertListEqual(sorted(data), merge_sort.merge_sort(data, 0, len(data)))
+        self.assertListEqual(sorted(data), merge_sort.sort(data))
 
     def test_merge_sort_chars(self):
         data = ['a', ',', 'K']
-        self.assertListEqual([',', 'K', 'a'], merge_sort.merge_sort(data, 0, len(data)))
+        self.assertListEqual([',', 'K', 'a'], merge_sort.sort(data))
 
     # Counting sort tests
 
     def test_counting_sort(self):
         data = ['a', ',', 'K']
-        self.assertListEqual([',', 'K', 'a'], counting_sort.counting_sort(data))
+        self.assertListEqual([',', 'K', 'a'], counting_sort.sort(data))

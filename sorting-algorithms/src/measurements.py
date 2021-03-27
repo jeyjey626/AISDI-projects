5326aa1eaef5e4668bc297731514201f4dd12a64
jeyjey626 <juliamakulec@gmail.com>
2021-03-27 18:35:00 +0100

    cleanup

--- sorting-algorithms/src/measurements.py
+++ sorting-algorithms/src/measurements.py
@@ -34,6 +34,7 @@ def measure_and_plot(array, function, title, file_name):
         x.append(i)
         y.append(t.timeit(1))  # todo increment repeat times after done testng
     plot(title, x, y, file_name)
+    update_user()
 
 
 if __name__ == '__main__':
@@ -41,11 +42,7 @@ if __name__ == '__main__':
     data = [ch for ch in f.read()]
     f.close()
     measure_and_plot(data, quick_sort, 'Sortowanie szybkie', 'quick_sort')
-    update_user()
     measure_and_plot(data, merge_sort, 'Sortowanie przez scalanie', 'merge_sort')
-    update_user()
     measure_and_plot(data, bubble_sort, 'Sortowanie bąbelkowe', 'bubble_sort')
-    update_user()
     measure_and_plot(data, counting_sort, 'Sortowanie przez zliczanie', 'counting_sort')
-    update_user()
     print('Pomiar zakończony, wykresy zapisane w folderze png-files')
